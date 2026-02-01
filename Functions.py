9ad8b8d71f7f974526377988fcd0a471954a5c46
Aditya Narayan Verma <adityaxxx2003@gmail.com>
2026-02-01 18:07:23 +0530

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,9 @@
+def calculate_square_and_cube(number):
+    square = number ** 2
+    cube = number ** 3
+    print(square)
+    print(cube)
+
+calculate_square_and_cube(2)
+calculate_square_and_cube(3)
+calculate_square_and_cube(4)
